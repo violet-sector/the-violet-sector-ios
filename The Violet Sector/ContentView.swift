18e3eefe1e98cfa4784210f876cb369417bfa5a0
João Santos <jps@xce.pt>
2020-08-01 04:42:37 +0100

    Initial Commit

---
+++ The Violet Sector/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  The Violet Sector
+//
+//  Created by João Santos on 01/08/2020.
+//  Copyright © 2020 João Santos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
