8ff67c50e35e5940a22b58b22ad3c88d2d324ec7
João Santos <jps@xce.pt>
2020-08-30 09:47:20 +0100

    Structural change to move the responsibility of showing the list content of the top pilots to its view.

--- The Violet Sector/TopPilotsView.swift
+++ The Violet Sector/TopPilotsView.swift
@@ -20,7 +20,24 @@ struct TopPilotsView: View {
                     .autocapitalization(.none)
                     .disableAutocorrection(true)
                 List(model.matches!, id: \.pilot.name) {(element) in
-                    PilotView(rank: element.rank, data: element.pilot)
+                    NavigationLink(destination: PilotView(rank: element.rank, data: element.pilot)) {
+                        HStack() {
+                            Text(verbatim: "\(element.rank)")
+                                .frame(width: 16.0)
+                            GeometryReader() {(geometry) in
+                                HStack(spacing: 0.0) {
+                                    (Text(verbatim: "\(element.pilot.name)\(element.pilot.isOnline ? "*" : "") [") + Text(verbatim: "\(element.pilot.legion.description.first!)").bold().foregroundColor(Color(.sRGB, red: element.pilot.legion.color.red, green: element.pilot.legion.color.green, blue: element.pilot.legion.color.blue, opacity: 1.0)) + Text(verbatim: "]"))
+                                        .frame(width: geometry.size.width * 0.5, alignment: .leading)
+                                    HealthView(current: element.pilot.currentHealth, max: element.pilot.maxHealth)
+                                        .percentage()
+                                        .frame(width: geometry.size.width * 0.2, alignment: .trailing)
+                                    Text(verbatim: "\(element.pilot.score > 10000 ? "\(element.pilot.score / 1000)k" : "\(element.pilot.score)") (\(element.pilot.level)")
+                                        .frame(width: geometry.size.width * 0.3, alignment: .trailing)
+                                }
+                            }
+                        }
+                    }
+                    .accessibilityElement(children: .combine)
                 }
             } else {
                 Spacer()
