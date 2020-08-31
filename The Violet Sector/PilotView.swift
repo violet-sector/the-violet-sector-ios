d7e4b9771ca4ae35ea0bdb1ceb5ac2013d3faa66
João Santos <jps@xce.pt>
2020-08-31 17:31:39 +0100

    Removed an unnecessary navigation view wrapper from pilot details.

--- The Violet Sector/PilotView.swift
+++ The Violet Sector/PilotView.swift
@@ -13,64 +13,63 @@ struct PilotView: View {
     var data: Pilot
 
     var body: some View {
-        NavigationView() {
-            GeometryReader() {(geometry) in
-                VStack() {
-                    Text(verbatim: "\(self.data.ship)")
-                    if self.rank > 0 {
-                        HStack(spacing: 20.0) {
-                            Text(verbatim: "Rank")
-                                .frame(width: geometry.size.width * 0.5 - 10.0, alignment: .trailing)
-                            Text(verbatim: "\(self.rank)")
-                                .frame(width: geometry.size.width * 0.5 - 10.0, alignment: .leading)
-                        }
-                        .accessibilityElement(children: .combine)
-                    }
+        GeometryReader() {(geometry) in
+            VStack() {
+                Text(verbatim: "\(self.data.ship)")
+                if self.rank > 0 {
                     HStack(spacing: 20.0) {
-                        Text(verbatim: "Legion")
+                        Text(verbatim: "Rank")
                             .frame(width: geometry.size.width * 0.5 - 10.0, alignment: .trailing)
-                        Text(verbatim: "\(self.data.legion)")
-                            .frame(width: geometry.size.width * 0.5 - 10.0, alignment: .leading)
-                    }
-                    .accessibilityElement(children: .combine)
-                    HStack(spacing: 20.0) {
-                        Text(verbatim: "Hitpoints")
-                            .frame(width: geometry.size.width * 0.5 - 10.0, alignment: .trailing)
-                        HealthView(current: self.data.currentHealth, max: self.data.maxHealth)
-                            .frame(width: geometry.size.width * 0.5 - 10.0, alignment: .leading)
-                    }
-                    .accessibilityElement(children: .combine)
-                    HStack(spacing: 20.0) {
-                        Text(verbatim: "Score")
-                            .frame(width: geometry.size.width * 0.5 - 10.0, alignment: .trailing)
-                        Text(verbatim: "\(self.data.score)")
-                            .frame(width: geometry.size.width * 0.5 - 10.0, alignment: .leading)
-                    }
-                    .accessibilityElement(children: .combine)
-                    HStack(spacing: 20.0) {
-                        Text(verbatim: "Level")
-                            .frame(width: geometry.size.width * 0.5 - 10.0, alignment: .trailing)
-                        Text(verbatim: "\(self.data.level)")
-                            .frame(width: geometry.size.width * 0.5 - 10.0, alignment: .leading)
-                    }
-                    .accessibilityElement(children: .combine)
-                    HStack(spacing: 20.0) {
-                        Text(verbatim: "Kills")
-                            .frame(width: geometry.size.width * 0.5 - 10.0, alignment: .trailing)
-                        Text(verbatim: "\(self.data.kills)")
-                            .frame(width: geometry.size.width * 0.5 - 10.0, alignment: .leading)
-                    }
-                    .accessibilityElement(children: .combine)
-                    HStack(spacing: 20.0) {
-                        Text(verbatim: "Deaths")
-                            .frame(width: geometry.size.width * 0.5 - 10.0, alignment: .trailing)
-                        Text(verbatim: "\(self.data.deaths)")
+                        Text(verbatim: "\(self.rank)")
                             .frame(width: geometry.size.width * 0.5 - 10.0, alignment: .leading)
                     }
                     .accessibilityElement(children: .combine)
                 }
+                HStack(spacing: 20.0) {
+                    Text(verbatim: "Legion")
+                        .frame(width: geometry.size.width * 0.5 - 10.0, alignment: .trailing)
+                    Text(verbatim: "\(self.data.legion)")
+                        .frame(width: geometry.size.width * 0.5 - 10.0, alignment: .leading)
+                }
+                .accessibilityElement(children: .combine)
+                HStack(spacing: 20.0) {
+                    Text(verbatim: "Hitpoints")
+                        .frame(width: geometry.size.width * 0.5 - 10.0, alignment: .trailing)
+                    HealthView(current: self.data.currentHealth, max: self.data.maxHealth)
+                        .frame(width: geometry.size.width * 0.5 - 10.0, alignment: .leading)
+                }
+                .accessibilityElement(children: .combine)
+                HStack(spacing: 20.0) {
+                    Text(verbatim: "Score")
+                        .frame(width: geometry.size.width * 0.5 - 10.0, alignment: .trailing)
+                    Text(verbatim: "\(self.data.score)")
+                        .frame(width: geometry.size.width * 0.5 - 10.0, alignment: .leading)
+                }
+                .accessibilityElement(children: .combine)
+                HStack(spacing: 20.0) {
+                    Text(verbatim: "Level")
+                        .frame(width: geometry.size.width * 0.5 - 10.0, alignment: .trailing)
+                    Text(verbatim: "\(self.data.level)")
+                        .frame(width: geometry.size.width * 0.5 - 10.0, alignment: .leading)
+                }
+                .accessibilityElement(children: .combine)
+                HStack(spacing: 20.0) {
+                    Text(verbatim: "Kills")
+                        .frame(width: geometry.size.width * 0.5 - 10.0, alignment: .trailing)
+                    Text(verbatim: "\(self.data.kills)")
+                        .frame(width: geometry.size.width * 0.5 - 10.0, alignment: .leading)
+                }
+                .accessibilityElement(children: .combine)
+                HStack(spacing: 20.0) {
+                    Text(verbatim: "Deaths")
+                        .frame(width: geometry.size.width * 0.5 - 10.0, alignment: .trailing)
+                    Text(verbatim: "\(self.data.deaths)")
+                        .frame(width: geometry.size.width * 0.5 - 10.0, alignment: .leading)
+                }
+                .accessibilityElement(children: .combine)
+                Spacer()
             }
-            .navigationBarTitle("\(data.name)\(data.isOnline ? "*" : "")", displayMode: .inline)
         }
+        .navigationBarTitle("\(data.name)\(data.isOnline ? "*" : "")", displayMode: .inline)
     }
 }
